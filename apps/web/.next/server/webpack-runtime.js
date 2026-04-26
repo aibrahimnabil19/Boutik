db28eb612aa6bd376874d3dc6a4cb52cf433c4ae
aibrahimnabil19 <aibrahimnabil@gmail.com>
2026-04-26 12:09:29 +0100

    Refactor jsconfig.json to remove baseUrl; add hot-update files for admin page and webpack

--- apps/web/.next/server/webpack-runtime.js
+++ apps/web/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8c9c2c2d108b8501")
+/******/ 		__webpack_require__.h = () => ("3f504d56aaa13b0d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f86b682bbcf009cd"; }
+/******/ 		__webpack_require__.h = function() { return "0451389e928fde14"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
